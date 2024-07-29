a4d3341d80c7aebc8cffccd51bb27e1eee352adb
emil-mordarski <124152747+emil-mordarski@users.noreply.github.com>
2024-07-29 22:46:53 +0200

    create order decrease products count

--- app/src/components/order/services/createOrder.ts
+++ app/src/components/order/services/createOrder.ts
@@ -1,22 +1,27 @@
 import { db } from "../../../config/db";
 import ConflictException from "../../../exception/ConflictException";
+import { decreaseProductStockLevel } from "../../stockManagement/services/decreaseProductStockLevel";
 import { OrderItem } from "../commands/CreateOrderCommand";
 import { addOrder } from "../repositories/addOrder";
 
 export async function createOrder(customerId: string, products: OrderItem[]) {
-	const productsInDb = db.data.products.filter(product => products.map(productInOrder => productInOrder.idś))
+	const productIds = products.map(productInOrder => productInOrder.productId)
+	const productsInDb = db.data.products.filter(product => productIds.includes(product.id))
 
-	const productsOutOfStock: string[] = []
+	const productIdsOutOfStock: string[] = []
 	for(let i=0; i<productsInDb.length; i++) {
-		if(productsInDb[i].count < products[i].count) {
-			productsOutOfStock.push(productsInDb[i].id)
+		if(productsInDb[i].stock < products[i].count) {
+			productIdsOutOfStock.push(productsInDb[i].id)
 		}
 	}
 
 	// * may add more details which product / count of available products
-	if(productsOutOfStock.length > 0) {
-		throw new ConflictException(`${productsOutOfStock.length} products are not available in stock`)
+	if(productIdsOutOfStock.length > 0) {
+		throw new ConflictException(`${productIdsOutOfStock.length} products are not available in stock`)
 	}
+
+	// * better solution to use 1 query to db for better performance
+	await Promise.all(productsInDb.map((product, i) => decreaseProductStockLevel(product.id, products[i].count)))
 
 	await addOrder(products);
 }
