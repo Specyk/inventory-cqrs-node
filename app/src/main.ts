153f8ad3f750f5d630ddb5bffcf654223884162a
emil-mordarski <124152747+emil-mordarski@users.noreply.github.com>
2024-07-29 14:46:20 +0200

    setup dotenv

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
 import { setupDb } from './config/db'
 import router from './components/router'
 
-
+dotenv.config()
 
 void async function main() {
 	await setupDb()
@@ -12,5 +14,5 @@ void async function main() {
 	const port = process.env.HTTP_SERVER_PORT
 	app.use(router)
 
-	app.listen(port, () => console.log(`App is listening on port ${port}!`))
+	app.listen(port, () => console.log(`App is listening on port ${port}`))
 }()
