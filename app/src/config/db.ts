385be3e39b1da9855606a851773df64f969de74d
emil-mordarski <124152747+emil-mordarski@users.noreply.github.com>
2024-07-29 14:49:42 +0200

    update db config

--- app/src/config/db.ts
+++ app/src/config/db.ts
@@ -1,6 +1,21 @@
+import { Low } from 'lowdb/lib/core/Low'
 import { JSONFilePreset } from 'lowdb/node'
 
 
+const initialData = {
+	products: [
+		{
+			name: 'first product',
+			description: 'first product description',
+			price: 10,
+			stock: 5
+		}
+	]
+}
+
+
+export let db: Low<typeof initialData>
+
 export async function setupDb() {
-	const db = await JSONFilePreset('../../data/db.json', { products: [] })
+	 db = await JSONFilePreset('../../data/db.json', initialData)
 }
